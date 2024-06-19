96a76d8bff0089ed791b89a37afc585f2da363e2
Julio Guerrero Alvarez <judaguer@espol.edu.ec>
2024-06-19 10:05:28 -0500

    actualizar dependencias

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,23 @@
-import { useState } from 'react'
 import Grid from '@mui/material/Unstable_Grid2'
 import './App.css'
+import Indicator from './components/Indicator'
+import Summary from './components/Summary'
+import BasicTable from './components/BasicTable'
 
 function App() {
 
   return (
     <Grid container spacing={5}>
-	      <Grid xs={12} sm={4} md={3} lg={2}>1</Grid>
-	      <Grid xs={6} sm={4} md={3} lg={2}>2</Grid>
-	      <Grid xs={6} sm={4} md={3} lg={2}>3</Grid>
-	      <Grid xs={12} sm={4} md={3} lg={2}>4</Grid>
-	      <Grid xs={6} sm={4} md={6} lg={2}>5</Grid>
-	      <Grid xs={6} sm={4} md={6} lg={2}>6</Grid>
-	    </Grid>
+	    <Grid xs={6} sm={4} md={3} lg={2}>
+			<Indicator title='Precipitación' subtitle='Probabilidad' value={0.13} />
+		</Grid>
+		<Grid xs={6} sm={4} md={3} lg={2}>
+	        <Summary></Summary>
+	    </Grid> 
+		<Grid xs={12} md={6} lg={9} >
+	       <BasicTable />
+	    </Grid>   
+	</Grid>
   )
 }
 
---
+++ src/components/Indicator.tsx
@@ -0,0 +1,26 @@
+import Typography from "@mui/material/Typography";
+import Paper from "@mui/material/Paper";
+import { Config } from "../interfaces/Config";
+
+
+export default function Indicator(config: Config) {
+  return (
+    <Paper
+      sx={{
+        p: 2,
+        display: "flex",
+        flexDirection: "column",
+      }}
+    >
+      <Typography component="h2" variant="h6" color="primary" gutterBottom>
+        {config.title}
+      </Typography>
+      <Typography component="p" variant="h4">
+        {config.value.toString()}
+      </Typography>
+      <Typography color="text.secondary" sx={{ flex: 1 }}>
+        {config.subtitle}
+      </Typography>
+    </Paper>
+  );
+}

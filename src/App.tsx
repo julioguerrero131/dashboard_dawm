ae53f1aeafafba136c958ac454ef0ce15c4953c8
Julio Guerrero Alvarez <judaguer@espol.edu.ec>
2024-06-19 10:32:21 -0500

    actividades guia 12

--- src/App.tsx
+++ src/App.tsx
@@ -8,13 +8,13 @@ function App() {
 
   return (
     <Grid container spacing={5}>
-	    <Grid xs={6} sm={4} md={3} lg={2}>
+	    <Grid xs={6} sm={4} md={3} lg={6}>
 			<Indicator title='Precipitación' subtitle='Probabilidad' value={0.13} />
 		</Grid>
-		<Grid xs={6} sm={4} md={3} lg={2}>
+		<Grid xs={6} sm={4} md={3} lg={6}>
 	        <Summary></Summary>
 	    </Grid> 
-		<Grid xs={12} md={6} lg={9} >
+		<Grid xs={12} md={6} lg={12} >
 	       <BasicTable />
 	    </Grid>   
 	</Grid>

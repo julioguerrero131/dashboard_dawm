1b69277ab350fb4492baa634959eca9a571f1b83
Julio Guerrero Alvarez <judaguer@espol.edu.ec>
2024-06-24 09:40:04 -0500

    agregar WheatherChart

--- src/App.tsx
+++ src/App.tsx
@@ -3,14 +3,19 @@ import './App.css'
 import Indicator from './components/Indicator'
 import Summary from './components/Summary'
 import BasicTable from './components/BasicTable'
+import WeatherChart from './components/WeatherChart'
 
 function App() {
 
   return (
     <Grid container spacing={5}>
 	    <Grid xs={6} sm={4} md={3} lg={2}>
-			<Indicator title='Precipitación' subtitle='Probabilidad' value={0.13}  />
-			<Summary></Summary>
+			<Grid lg={12}>
+				<Indicator title='Precipitación' subtitle='Probabilidad' value={0.13}  />
+			</Grid>
+			<Grid lg={12}>
+				<Summary></Summary>
+			</Grid>
 		</Grid>
 		<Grid xs={6} sm={4} md={3} lg={2}>
 			<Indicator title='Precipitación' subtitle='Probabilidad' value={0.13} />
@@ -33,7 +38,11 @@ function App() {
 	    </Grid> 
 		<Grid xs={12} md={6} lg={12} >
 	       <BasicTable />
-	    </Grid>   
+	    </Grid>  
+
+		<Grid xs={12} lg={10}>
+            <WeatherChart></WeatherChart>
+         </Grid> 
 	</Grid>
   )
 }

851c4ef8f68a046e962356703cf350bcd20496ab
Julio Guerrero Alvarez <judaguer@espol.edu.ec>
2024-07-08 23:46:14 -0500

    guia 14 terminada

--- src/components/BasicTable.tsx
+++ src/components/BasicTable.tsx
@@ -5,51 +5,65 @@ import TableContainer from '@mui/material/TableContainer';
 import TableHead from '@mui/material/TableHead';
 import TableRow from '@mui/material/TableRow';
 import Paper from '@mui/material/Paper';
+import { useState, useEffect } from 'react';
 
-function createData(
-  name: string,
-  calories: number,
-  fat: number,
-  carbs: number,
-  protein: number,
-) {
-  return { name, calories, fat, carbs, protein };
+// function createData(
+//   name: string,
+//   calories: number,
+//   fat: number,
+//   carbs: number,
+//   protein: number,
+// ) {
+//   return { name, calories, fat, carbs, protein };
+// }
+
+// const rows = [
+//   createData('Frozen yoghurt', 159, 6.0, 24, 4.0),
+//   createData('Ice cream sandwich', 237, 9.0, 37, 4.3),
+//   createData('Eclair', 262, 16.0, 24, 6.0),
+//   createData('Cupcake', 305, 3.7, 67, 4.3),
+//   createData('Gingerbread', 356, 16.0, 49, 3.9),
+// ];
+
+interface Config {
+  rows: Array<object>;
 }
 
-const rows = [
-  createData('Frozen yoghurt', 159, 6.0, 24, 4.0),
-  createData('Ice cream sandwich', 237, 9.0, 37, 4.3),
-  createData('Eclair', 262, 16.0, 24, 6.0),
-  createData('Cupcake', 305, 3.7, 67, 4.3),
-  createData('Gingerbread', 356, 16.0, 49, 3.9),
-];
+export default function BasicTable(data: Config) {
 
-export default function BasicTable() {
+  let [rows, setRows] = useState([])
+
+  useEffect(() => {
+
+    (() => {
+
+      setRows(data.rows)
+
+    })()
+
+  }, [data])
+
+  {/* JSX */ }
+
   return (
     <TableContainer component={Paper}>
       <Table sx={{ minWidth: 650 }} aria-label="simple table">
         <TableHead>
           <TableRow>
-            <TableCell>Dessert (100g serving)</TableCell>
-            <TableCell align="right">Calories</TableCell>
-            <TableCell align="right">Fat&nbsp;(g)</TableCell>
-            <TableCell align="right">Carbs&nbsp;(g)</TableCell>
-            <TableCell align="right">Protein&nbsp;(g)</TableCell>
+            <TableCell>Rango de horas</TableCell>
+            <TableCell align="right">Dirección del viento</TableCell>
           </TableRow>
         </TableHead>
         <TableBody>
           {rows.map((row) => (
             <TableRow
-              key={row.name}
+              key={row.rangeHours}
               sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
             >
               <TableCell component="th" scope="row">
-                {row.name}
+                {row.rangeHours}
               </TableCell>
-              <TableCell align="right">{row.calories}</TableCell>
-              <TableCell align="right">{row.fat}</TableCell>
-              <TableCell align="right">{row.carbs}</TableCell>
-              <TableCell align="right">{row.protein}</TableCell>
+              <TableCell align="right">{row.windDirection}</TableCell>
             </TableRow>
           ))}
         </TableBody>
--- src/components/WeatherChart.tsx
+++ src/components/WeatherChart.tsx
@@ -3,45 +3,45 @@ import Paper from '@mui/material/Paper';
 
 export default function WeatherChart() {
 
-    {/* Configuración */}
+  {/* Configuración */ }
 
-    let options = {
-        title: "Precipitación, Humedad y Nubosidad vs Hora",
-        curveType: "function",
-        legend: { position: "right" },
-    }
+  let options = {
+    title: "Precipitación, Humedad y Nubosidad vs Hora",
+    curveType: "function",
+    legend: { position: "right" },
+  }
 
-    {/* Datos de las variables meteorológicas */}
+  {/* Datos de las variables meteorológicas */ }
 
-    const data = [
-        ["Hora", "Precipitación", "Humedad", "Nubosidad"],
-        ["03:00", 13, 78, 75],
-        ["06:00", 4, 81, 79],
-        ["09:00", 7, 82, 69],
-        ["12:00", 3, 73, 62],
-        ["15:00", 4, 66, 75],
-        ["18:00", 6, 64, 84],
-        ["21:00", 5, 77, 99]
-    ];
+  const data = [
+    ["Hora", "Precipitación", "Humedad", "Nubosidad"],
+    ["03:00", 13, 78, 75],
+    ["06:00", 4, 81, 79],
+    ["09:00", 7, 82, 69],
+    ["12:00", 3, 73, 62],
+    ["15:00", 4, 66, 75],
+    ["18:00", 6, 64, 84],
+    ["21:00", 5, 77, 99]
+  ];
 
-    {/* JSX */}
+  {/* JSX */ }
 
-    return (
-        <Paper
-            sx={{
-                p: 2,
-                display: 'flex',
-                flexDirection: 'column'
-            }}
-        >
-            <Chart
-                chartType="LineChart"
-                data={data}
-                width="100%"
-                height="400px"
-                options={options}
-                legendToggle
-        />
-        </Paper>
-    )
+  return (
+    <Paper
+      sx={{
+        p: 2,
+        display: 'flex',
+        flexDirection: 'column'
+      }}
+    >
+      <Chart
+        chartType="LineChart"
+        data={data}
+        width="100%"
+        height="400px"
+        options={options}
+        legendToggle
+      />
+    </Paper>
+  )
 }	
